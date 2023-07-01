2b60320b8b98b4852fe76de15aeba01fa9525d99
Cris-aian Vergara <cris.vergara@achievewithoutborders.com>
2023-07-01 14:07:53 +0800

    First commit!

---
+++ money_lending/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Money Lending',
+    'version': '1.0.0',
+    'category': 'Finance',
+    'author': 'Cris-aian Vergara',
+    'sequence': -100,
+    'summary': 'Money lending',
+    'description': """Money lending""",
+    'depends': ['base'],
+    'data': [
+    ],
+    'demo': [],
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
