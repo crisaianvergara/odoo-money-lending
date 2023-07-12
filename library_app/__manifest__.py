925057311a4d4a72d50dc572048a698ab63fcd7c
Cris-aian Vergara <cris.vergara@achievewithoutborders.com>
2023-07-12 22:28:14 +0800

    Started checkout feature.

--- library_app/__manifest__.py
+++ library_app/__manifest__.py
@@ -15,6 +15,7 @@
         "views/book_view.xml",
         "views/library_menu.xml",
         "views/book_list_template.xml",
+        "reports/library_book_report.xml",
     ],
     "demo": [
         "data/res.partner.csv",
